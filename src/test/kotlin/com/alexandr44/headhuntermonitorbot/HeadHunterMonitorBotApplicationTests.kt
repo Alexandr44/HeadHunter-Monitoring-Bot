47dfe4356bb48c271f469e5e705d879f3f5815a9
Alexandr Glukhov <alexandr.glukhov@betoola.com>
2025-11-13 02:48:24 +0500

    Init project, added dependencies

---
+++ src/test/kotlin/com/alexandr44/headhuntermonitorbot/HeadHunterMonitorBotApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.alexandr44.headhuntermonitorbot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HeadHunterMonitorBotApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
