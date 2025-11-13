01de0ad23ed5b99619d2bd8a561d638b9bfd26c1
Alexandr Glukhov <alexandr.glukhov@betoola.com>
2025-11-13 10:03:04 +0500

    Added annotation

--- src/main/kotlin/com/alexandr44/headhuntermonitorbot/HeadHunterMonitorBotApplication.kt
+++ src/main/kotlin/com/alexandr44/headhuntermonitorbot/HeadHunterMonitorBotApplication.kt
@@ -3,9 +3,11 @@ package com.alexandr44.headhuntermonitorbot
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @EnableScheduling
+@EnableJpaAuditing
 @EnableFeignClients
 @SpringBootApplication
 class HeadHunterMonitorBotApplication {
