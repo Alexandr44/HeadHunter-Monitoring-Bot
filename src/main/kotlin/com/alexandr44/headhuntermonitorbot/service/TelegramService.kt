ce696b0a477fcbcf1f5bd21806f4c1ec20bb0e72
Alexandr Glukhov <alexandr.glukhov@betoola.com>
2025-11-20 20:12:03 +0500

    Added request button to vacancy

--- src/main/kotlin/com/alexandr44/headhuntermonitorbot/service/TelegramService.kt
+++ src/main/kotlin/com/alexandr44/headhuntermonitorbot/service/TelegramService.kt
@@ -1,9 +1,12 @@
 package com.alexandr44.headhuntermonitorbot.service
 
-import com.alexandr44.headhuntermonitorbot.dto.VacancyDto
+import com.alexandr44.headhuntermonitorbot.dto.response.VacancyDto
+import com.alexandr44.headhuntermonitorbot.enums.Callback
 import org.springframework.stereotype.Service
 import org.telegram.telegrambots.bots.TelegramLongPollingBot
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton
 
 @Service
 class TelegramService(
@@ -36,7 +39,24 @@ class TelegramService(
 
         val msg = SendMessage(chatId.toString(), message)
         msg.parseMode = "Markdown"
+        msg.replyMarkup = buildReplyButton(vacancyDto)
         telegramBot.execute(msg)
+    }
+
+    private fun buildReplyButton(vacancyDto: VacancyDto): InlineKeyboardMarkup {
+        val row = mutableListOf<InlineKeyboardButton>()
+
+        if (vacancyDto.hasTest) {
+            row += InlineKeyboardButton("🚫 Недоступно - есть тест").apply {
+                callbackData = "none"
+            }
+        } else {
+            row += InlineKeyboardButton("Откликнуться").apply {
+                callbackData = "${Callback.VACANCY}:${vacancyDto.id}"
+            }
+        }
+
+        return InlineKeyboardMarkup(listOf(row))
     }
 
 }
