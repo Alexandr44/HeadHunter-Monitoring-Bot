93340be06f5715da5b750077301c494dc86e1065
Alexandr Glukhov <alexandr.glukhov@betoola.com>
2025-11-20 19:43:17 +0500

    Added hh methods

--- src/main/kotlin/com/alexandr44/headhuntermonitorbot/client/HeadHunterClient.kt
+++ src/main/kotlin/com/alexandr44/headhuntermonitorbot/client/HeadHunterClient.kt
@@ -1,14 +1,22 @@
 package com.alexandr44.headhuntermonitorbot.client
 
-import com.alexandr44.headhuntermonitorbot.dto.VacancyResponseDto
+import com.alexandr44.headhuntermonitorbot.dto.TokenRefreshRequest
+import com.alexandr44.headhuntermonitorbot.dto.TokenRequest
+import com.alexandr44.headhuntermonitorbot.dto.response.ResumeDto
+import com.alexandr44.headhuntermonitorbot.dto.response.TokenDto
+import com.alexandr44.headhuntermonitorbot.dto.response.VacancyResponseDto
+import feign.Headers
 import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.cloud.openfeign.SpringQueryMap
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.*
+
 
 @FeignClient(
     name = "my-service",
-    url = "https://api.hh.ru"
+    url = "https://api.hh.ru",
 )
 interface HeadHunterClient {
 
@@ -20,5 +28,22 @@ interface HeadHunterClient {
         @RequestParam("per_page") perPage: Int,
         @RequestParam("page") page: Int
     ): ResponseEntity<VacancyResponseDto>
+
+    @PostMapping(
+        value = ["/token"],
+        consumes = [MediaType.APPLICATION_FORM_URLENCODED_VALUE]
+    )
+    @Headers("${HttpHeaders.CONTENT_TYPE}: ${MediaType.APPLICATION_FORM_URLENCODED_VALUE}")
+    fun requestToken(@SpringQueryMap body: TokenRequest): ResponseEntity<TokenDto>
+
+    @PostMapping("/token")
+    @Headers("${HttpHeaders.CONTENT_TYPE}: ${MediaType.APPLICATION_FORM_URLENCODED_VALUE}")
+    fun refreshToken(@SpringQueryMap body: TokenRefreshRequest): ResponseEntity<TokenDto>
+
+    @GetMapping("/resumes/{resumeId}")
+    fun getResumeById(
+        @PathVariable("resumeId") resumeId: String,
+        @RequestHeader("Authorization") bearerToken: String
+    ): ResponseEntity<ResumeDto>
 
 }
--- src/main/kotlin/com/alexandr44/headhuntermonitorbot/dto/TokenRefreshRequest.kt
+++ src/main/kotlin/com/alexandr44/headhuntermonitorbot/dto/TokenRefreshRequest.kt
@@ -2,14 +2,13 @@ package com.alexandr44.headhuntermonitorbot.dto
 
 import feign.Param
 
-class TokenRequest(
+class TokenRefreshRequest(
     @get:Param("grant_type")
     val grantType: String,
     @get:Param("client_id")
     val clientId: String,
     @get:Param("client_secret")
     val clientSecret: String,
-    val code: String,
-    @get:Param("redirect_uri")
-    val redirectUri: String
+    @get:Param("refresh_token")
+    val refreshToken: String
 )
