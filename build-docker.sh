600f4aa25411238b86276a8aee8fea2715a128f0
Alexandr Glukhov <alexandr.glukhov@betoola.com>
2025-11-24 00:47:16 +0500

    Fixed configs

--- build-docker.sh
+++ build-docker.sh
@@ -2,3 +2,4 @@
 docker build -t headhuntermonitoringbot:1.0 .
 docker save headhuntermonitoringbot:1.0 -o headhuntermonitoringbot.tar
 scp ./headhuntermonitoringbot.tar mediaserver@192.168.0.200:/tmp/
+rm ./headhuntermonitoringbot.tar
